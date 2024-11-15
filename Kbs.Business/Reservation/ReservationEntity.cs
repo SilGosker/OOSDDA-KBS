289836443cbed14e35553b6e6aa616688f87c7f4
Sietse <sietsedrent@gmail.com>
2024-11-15 10:30:04 +0100

    Test
    Simpele attributen

---
+++ Kbs.Business/Reservation/ReservationEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbs.Business.Reservation
+{
+    internal class ReservationEntity
+    {
+        private int reservationId;
+        private DateTime startDate;
+        private ReservationStatus status;
+        private int userId;
+        //private Reservationniveau Niveau;
+        private bool isForCompetition;
+        private int tijdsduur;
+        //private DateTime EndDate;
+    }
+}
---
+++ Kbs.Business/Reservation/ReservationStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbs.Business.Reservation
+{
+    internal class ReservationStatus
+    {
+        public enum reservationStatus
+        {
+           Verwijderd = 1,
+           Verlopen = 2,
+           Actief = 3
+        };
+    }
+}

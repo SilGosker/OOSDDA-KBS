82426be36d74bd08726e2dbd1a364ab13ba4d97b
Sil Gosker <silgosker@gmail.com>
2024-11-18 15:03:51 +0100

    add boat and boat type logic

---
+++ Kbs.Business/Boat/BoatEntity.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Formats.Tar;
+
+namespace Kbs.Business.Boat;
+
+public class BoatEntity
+{
+    public int BoatID { get; set; }
+    public string Name { get; set; }
+    public int BoatTypeId { get; set; }
+    public BoatStatus Status { get; set; }
+}
---
+++ Kbs.Business/BoatType/BoatTypeEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Kbs.Business.BoatType;
+
+public class BoatTypeEntity
+{
+    public int BoatTypeID { get; set; }
+    public string Name { get; set; }
+    public int RequiredExperience { get; set; }
+    public int Speed { get; set; }
+    public int Seats { get; set; }
+    public bool HasSteeringWheel { get; set; }
+}

6e849cde2a55b1604bec8c8b0ad937296bc9ece5
Sil Gosker <silgosker@gmail.com>
2024-11-18 17:27:24 +0100

    Add unit tests

---
+++ Kbs.Business.Tests/BoatType/BoatTypeEntityTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Kbs.Business.BoatType;
+
+public class BoatTypeEntityTests
+{
+
+    [Theory]
+    public void Properties_ShouldSetValues(int id, string name, BoatTypeRequiredExperience experience, int seats, int speed, bool hasSteeringWheel)
+    {
+        // Arrange
+        var boatType = new BoatTypeEntity();
+
+        // Act
+        boatType.BoatTypeID = id;
+        boatType.Name = name;
+        boatType.RequiredExperience = experience;
+        boatType.Seats = seats;
+        boatType.Speed = speed;
+        boatType.HasSteeringWheel = hasSteeringWheel;
+
+        // Assert
+        Assert.Equal(boatType.BoatTypeID, id);
+        Assert.Equal(boatType.Name, name);
+        Assert.Equal(boatType.RequiredExperience, experience);
+        Assert.Equal(boatType.Seats, seats);
+        Assert.Equal(boatType.Speed, speed);
+        Assert.Equal(boatType.HasSteeringWheel, hasSteeringWheel);
+    }
+}

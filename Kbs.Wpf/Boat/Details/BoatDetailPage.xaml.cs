ba713cebdc17d40addce2bc95f94d897156d497d
Sil Gosker <silgosker@gmail.com>
2024-11-19 16:17:15 +0100

    Add data binding

--- Kbs.Wpf/Boat/Details/BoatDetailPage.xaml.cs
+++ Kbs.Wpf/Boat/Details/BoatDetailPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows.Controls;
+using Kbs.Business.Boat;
 using Kbs.Business.User;
+using Kbs.Data.Boat;
+using Kbs.Data.User;
 using Kbs.Wpf.Attributes;
 
 namespace Kbs.Wpf.Boat.Details;
@@ -7,8 +10,22 @@ namespace Kbs.Wpf.Boat.Details;
 [HasRole(Role.MaterialCommissioner)]
 public partial class BoatDetailPage : Page
 {
+    private readonly IBoatRepository _boatRepository = new BoatRepository();
+    private readonly IUserRepository _userRepository = new UserRepository();
+    public BoatDetailViewModel ViewModel => (BoatDetailViewModel)DataContext;
     public BoatDetailPage(int boatId)
     {
         InitializeComponent();
+        var boat = _boatRepository.GetById(boatId);
+        ViewModel.Name = boat.Name;
+        ViewModel.Status = boat.Status switch
+        {
+            BoatStatus.Operational => "Operationeel",
+            BoatStatus.Maintaining => "In onderhoud",
+            BoatStatus.Broken => "Kapot",
+            _ => "Onbekend"
+        };
+
+        ViewModel.BoatTypeName = "Onbekend";
     }
 }

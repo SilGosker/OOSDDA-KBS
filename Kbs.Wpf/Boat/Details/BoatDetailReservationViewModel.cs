f33cdee646610ecad2d51ae0c9d25c25f54e3ae4
Sil Gosker <silgosker@gmail.com>
2024-11-19 12:38:25 +0100

    Added views for boatDetail

---
+++ Kbs.Wpf/Boat/Details/BoatDetailReservationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Kbs.Wpf.Components;
+
+namespace Kbs.Wpf.Boat.Details;
+
+public class BoatDetailReservationViewModel : ViewModel
+{
+    public BoatDetailReservationViewModel()
+    {
+
+    }
+
+    public string UserName { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+    public string Status { get; set; }
+    public int ReservationId { get; set; }
+}
---
+++ Kbs.Wpf/Boat/Details/BoatDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using Kbs.Wpf.Components;
+
+namespace Kbs.Wpf.Boat.Details;
+
+public class BoatDetailViewModel : ViewModel
+{
+    public ObservableCollection<BoatDetailReservationViewModel> Reservations { get; } = new();
+    public int BoatId { get; set; }
+    public string Name { get; set; }
+    public string Status { get; set; }
+    public string BoatTypeName { get; set; }
+}

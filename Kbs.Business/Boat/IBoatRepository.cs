330d83a871fea43f5fed08135b87711b19da96b6
Sil Gosker <silgosker@gmail.com>
2024-11-19 16:17:08 +0100

    Add GetById method to BoatRepository interface

--- Kbs.Business/Boat/IBoatRepository.cs
+++ Kbs.Business/Boat/IBoatRepository.cs
@@ -6,4 +6,5 @@ public interface IBoatRepository
     List<BoatEntity> GetManyByType(int boatTypeId);
     List<BoatEntity> GetManyByName(string name);
     List<BoatEntity> GetManyByNameAndType(string name, int boatTypeId);
+    BoatEntity GetById(int boatId);
 }

a213eb526dd38846d61b79d2faaa5b1e6b978ba7
Natan den Hartog <natan2004@live.nl>
2024-11-25 12:32:38 +0100

    add page to update boattype

---
+++ Kbs.Wpf/BoatType/Update/UpdateBoatTypeViewModel.cs
@@ -0,0 +1,84 @@
+using System.Collections.ObjectModel;
+using Kbs.Business.BoatType;
+using Kbs.Wpf.BoatType.CreateBoatType;
+using Kbs.Wpf.Components;
+
+namespace Kbs.Wpf.BoatType.Update;
+
+public class UpdateBoatTypeViewModel : ViewModel
+{
+    private string _name;
+    private BoatTypeRequiredExperience _requiredExperience;
+    private BoatTypeSeats _seats;
+    private int _speed;
+    private bool _hasSteeringWheel;
+    private string _experienceErrorMessage;
+    private string _speedErrorMessage;
+    private string _seatsErrorMessage;
+    private CreateBoatExperienceViewModel _selectedExperience;
+    private CreateBoatSeatsViewModel _selectedSeats;
+    
+    
+    public ObservableCollection<CreateBoatExperienceViewModel> PossibleExperiences { get; } = [];
+    public ObservableCollection<CreateBoatSeatsViewModel> PossibleSeats { get; } = [];
+
+    public string Name
+    {
+        get => _name;
+        set => SetField(ref _name, value);
+    }
+
+    public BoatTypeRequiredExperience RequiredExperience
+    {
+        get => _requiredExperience;
+        set => SetField(ref _requiredExperience, value);
+    }
+
+    public int Speed
+    {
+        get => _speed;
+        set => SetField(ref _speed, value);
+    }
+
+    public BoatTypeSeats Seats
+    {
+        get => _seats;
+        set => SetField(ref _seats, value);
+    }
+
+    public bool HasSteeringWheel
+    {
+        get => _hasSteeringWheel;
+        set => SetField(ref _hasSteeringWheel, value);
+    }
+
+    public string ExperienceErrorMessage
+    {
+        get => _experienceErrorMessage;
+        set => SetField(ref _experienceErrorMessage, value);
+    }
+
+    public string SpeedErrorMessage
+    {
+        get => _speedErrorMessage;
+        set => SetField(ref _speedErrorMessage, value);
+    }
+
+    public string SeatsErrorMessage
+    {
+        get => _seatsErrorMessage;
+        set => SetField(ref _seatsErrorMessage, value);
+    }
+    
+    public CreateBoatExperienceViewModel SelectedExperience
+    {
+        get => _selectedExperience;
+        set => SetField(ref _selectedExperience, value);
+    }
+
+    public CreateBoatSeatsViewModel SelectedSeats
+    {
+        get => _selectedSeats;
+        set => SetField(ref _selectedSeats, value);
+    }
+}

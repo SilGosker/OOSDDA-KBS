e3be916f144f3cf0476aed760fa2a498bdd1c581
Sil Gosker <silgosker@gmail.com>
2024-12-03 08:55:04 +0100

    add viewmodel

---
+++ Kbs.Wpf/Damage/Upload/UploadDamageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Kbs.Wpf.Components;
+
+namespace Kbs.Wpf.Damage.Upload;
+
+public class UploadDamageViewModel : ViewModel
+{
+    private string _description;
+    private bool _boatStatusShouldChange;
+    private string _imagePath;
+    private string _fileErrorMessage;
+    private string _descriptionErrorMessage;
+    public string Description
+    {
+        get => _description;
+        set => SetField(ref _description, value);
+    }
+
+    public bool BoatStatusShouldChange
+    {
+        get => _boatStatusShouldChange;
+        set => SetField(ref _boatStatusShouldChange, value);
+    }
+
+    public string ImagePath
+    {
+        get => _imagePath;
+        set => SetField(ref _imagePath, value);
+    }
+
+    public string FileErrorMessage
+    {
+        get => _fileErrorMessage;
+        set => SetField(ref _fileErrorMessage, value);
+    }
+
+    public string DescriptionErrorMessage
+    {
+        get => _descriptionErrorMessage;
+        set => SetField(ref _descriptionErrorMessage, value);
+    }
+}

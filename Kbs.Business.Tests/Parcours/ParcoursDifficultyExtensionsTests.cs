8634b371a02641f3514b2495aa885c7fe0e37901
Sil Gosker <silgosker@gmail.com>
2024-12-04 17:13:47 +0100

    add unit tests

---
+++ Kbs.Business.Tests/Parcours/ParcoursDifficultyExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Kbs.Business.Parcours;
+
+public class ParcoursDifficultyExtensionsTests
+{
+    [Theory]
+    [InlineData(ParcoursDifficulty.Easy, "Makkelijk")]
+    [InlineData(ParcoursDifficulty.Medium, "Gemiddeld")]
+    [InlineData(ParcoursDifficulty.Hard, "Moeilijk")]
+    [InlineData(ParcoursDifficulty.VeryHard, "Heel moeilijk")]
+    [InlineData((ParcoursDifficulty)int.MaxValue, "Onbekend")]
+    [InlineData((ParcoursDifficulty)0, "Onbekend")]
+    public void ToDutchString_TranslatesValue(ParcoursDifficulty difficulty, string text)
+    {
+        // Assert & Act
+        var result = difficulty.ToDutchString();
+
+        // Assert
+        Assert.Equal(text, result);
+    }
+}
---
+++ Kbs.Business.Tests/Parcours/ParcoursEntityTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Kbs.Business.Parcours;
+
+public class ParcoursEntityTests
+{
+    [Theory]
+    [InlineData(1, new byte[] { 1, 2, 3 }, "Parcours 1", "Description 1", ParcoursDifficulty.Easy)]
+    [InlineData(int.MaxValue, new byte[] { }, "", "", (ParcoursDifficulty)int.MaxValue)]
+    [InlineData(int.MinValue, null, null, null, (ParcoursDifficulty)0)]
+    public void Properties_ShouldSetProperties(int id, byte[] image, string name, string description,
+        ParcoursDifficulty difficulty)
+
+    {
+        // Arrange
+        var parcours = new ParcoursEntity();
+
+        // Act
+        parcours.ParcoursId = id;
+        parcours.Image = image;
+        parcours.Name = name;
+        parcours.Description = description;
+        parcours.Difficulty = difficulty;
+
+        // Assert
+        Assert.Equal(id, parcours.ParcoursId);
+        Assert.Equal(image, parcours.Image);
+        Assert.Equal(name, parcours.Name);
+        Assert.Equal(description, parcours.Description);
+        Assert.Equal(difficulty, parcours.Difficulty);
+    }
+}

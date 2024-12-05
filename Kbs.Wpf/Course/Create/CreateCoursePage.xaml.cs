a65b1b9db4d4294c142492681767a80c2bf9bc41
ColinvD <vanDongen.colin@gmail.com>
2024-12-05 11:25:54 +0100

    progress commit

---
+++ Kbs.Wpf/Course/Create/CreateCoursePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kbs.Wpf.Course.Create
+{
+    /// <summary>
+    /// Interaction logic for CreateCoursePage.xaml
+    /// </summary>
+    public partial class CreateCoursePage : Page
+    {
+        public CreateCoursePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kbs.Wpf/Course/Create/CreateCourseViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Kbs.Wpf.Components;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Kbs.Wpf.Course.Create
+{
+    public class CreateCourseViewModel : ViewModel
+    {
+        private string _name;
+        private string _nameErrorMessage;
+        private string _imagePath;
+        private ImageSource _image;
+        private string _imageErrorMessage;
+        private CourseDifficulty _difficulty;
+        private string _description;
+        public ObservableCollection<CourseDifficulty> PossibleDifficulties { get; } = new();
+        public string Name 
+        { 
+            get => _name; 
+            set => SetField(ref _name, value); 
+        }
+        public string NameErrorMessage 
+        { 
+            get => _nameErrorMessage;
+            set => SetField(ref _nameErrorMessage, value);
+        }
+        public string ImagePath
+        {
+            get => _imagePath;
+            set
+            {
+                SetField(ref _imagePath, value);
+                OnPropertyChanged(nameof(Image));
+            }
+        }
+
+        public ImageSource Image
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(ImagePath))
+                {
+                    return null;
+                }
+
+                return new BitmapImage(new Uri(ImagePath));
+
+            }
+        }
+
+        public string ImageErrorMessage 
+        { 
+            get => _imageErrorMessage;
+            set => SetField(ref _imageErrorMessage, value);
+        }
+        public CourseDifficulty Difficulty 
+        { 
+            get => _difficulty;
+            set => SetField(ref _difficulty, value);
+        }
+        public string Description 
+        { 
+            get => _description;
+            set => SetField(ref _description, value);
+        }
+    }
+}

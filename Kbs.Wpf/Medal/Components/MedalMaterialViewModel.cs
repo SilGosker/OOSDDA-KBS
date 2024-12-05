4acd5468225b293477d77a41847a20fb544d18b1
turtleman31 <146923602+turtleman31@users.noreply.github.com>
2024-12-05 15:33:16 +0100

    progress comit

---
+++ Kbs.Wpf/Medal/Components/MedalMaterialViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Kbs.Business.Boat;
+using Kbs.Business.Medal;
+using Kbs.Wpf.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbs.Wpf.Medal.Components
+{
+    public class MedalMaterialViewModel : ViewModel
+    {
+        public MedalMaterialViewModel(MedalMaterial medalMaterial)
+        {
+            this.MedalMaterial = medalMaterial;
+        }
+
+        public MedalMaterial MedalMaterial { get; }
+
+        // Override ToString to display the BoatStatuses in Dutch
+        public override string ToString()
+        {
+            return MedalMaterial.ToDutchString();
+        }
+    }
+}
+

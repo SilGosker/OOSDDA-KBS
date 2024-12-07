4444ac6b3fb5e2212373edce59c909bb5bf46b26
Natan den Hartog <natan2004@live.nl>
2024-12-07 11:12:13 +0100

    add methods to gamerepository

--- Kbs.Business/Game/IGameRepository.cs
+++ Kbs.Business/Game/IGameRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Kbs.Business.Game;
+namespace Kbs.Business.Game;
 
-public interface IGameRepository
-{
-    public void Create(GameEntity game);
-}
+ public interface IGameRepository
+ {
+     public void Create(GameEntity game);
+     public GameEntity GetById(int gameId);
+     public List<GameEntity> GetMany();
+     public List<GameEntity> GetManyByCourse(int courseId);
+     public List<GameEntity> GetManyByName(string name);
+     public List<GameEntity> GetManyByNameAndCourse(string name, int courseId);
+     public void DeleteById(int gameId);
+     public void Update(GameEntity game);
+ }
--- Kbs.Data/Game/GameRepository.cs
+++ Kbs.Data/Game/GameRepository.cs
@@ -1,4 +1,4 @@
-﻿using Dapper;
+using Dapper;
 using Kbs.Business.Game;
 using Microsoft.Data.SqlClient;
 
@@ -7,8 +7,45 @@ namespace Kbs.Data.Game;
 public class GameRepository : IGameRepository
 {
     private readonly SqlConnection _connection = new(DatabaseConstants.ConnectionString);
+
     public void Create(GameEntity game)
     {
-        game.GameId = _connection.QuerySingle<int>("INSERT INTO Game (Name, Date, CourseID) VALUES (@Name, @Date, @CourseId); SELECT SCOPE_IDENTITY();", game);
+        game.GameId = _connection.QuerySingle<int>(
+            "INSERT INTO Game (Name, Date, CourseID) VALUES (@Name, @Date, @CourseId); SELECT SCOPE_IDENTITY();", game);
+    }
+
+    public GameEntity GetById(int gameId)
+    {
+        return _connection.QueryFirstOrDefault<GameEntity>("SELECT * FROM Game WHERE GameID = @gameId", new { gameId });
+    }
+
+    public List<GameEntity> GetMany()
+    {
+        return _connection.Query<GameEntity>("SELECT * FROM Game").ToList();
+    }
+
+    public List<GameEntity> GetManyByCourse(int courseId)
+    {
+        return _connection.Query<GameEntity>("SELECT * FROM Game WHERE CourseID = @courseId", new { courseId }).ToList();
+    }
+
+    public List<GameEntity> GetManyByName(string name)
+    {
+        return _connection.Query<GameEntity>("SELECT * FROM Game WHERE LOWER(Name) LIKE @name", new { name = $"%{name.ToLower()}%" }).ToList();
+    }
+
+    public List<GameEntity> GetManyByNameAndCourse(string name, int courseId)
+    {
+        return _connection.Query<GameEntity>("SELECT * FROM Game WHERE LOWER(Name) LIKE @name AND CourseID = @courseId", new { name = $"%{name.ToLower()}%", courseId }).ToList();
+    }
+
+    public void DeleteById(int gameId)
+    {
+        _connection.Execute("DELETE FROM Game WHERE GameId = @gameId", new { gameId });
+    }
+
+    public void Update(GameEntity game)
+    {
+        _connection.Execute("UPDATE Game SET CourseID = @CourseId, Name = @Name, Date = @Date WHERE GameId = @GameId", game);
     }
 }

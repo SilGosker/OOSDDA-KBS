6d323a651f37af4b10006356df8ebb7b2c74ade7
Sil Gosker <silgosker@gmail.com>
2024-12-11 14:04:15 +0100

    removed comparator to old email

--- Kbs.Business/User/UserValidator.cs
+++ Kbs.Business/User/UserValidator.cs
@@ -7,8 +7,10 @@ namespace Kbs.Business.User;
 public class UserValidator
 {
     private static readonly Regex EmailValidationRegex =
-        new Regex("^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$", RegexOptions.Compiled);
-    
+        new Regex(
+            "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$",
+            RegexOptions.Compiled);
+
     public Dictionary<string, string> ValidatorForLogIn(UserEntity user)
     {
         ThrowHelper.ThrowIfNull(user);
@@ -34,7 +36,9 @@ public class UserValidator
 
         return errors;
     }
-    public Dictionary<string, string> ValidatorForUpdate(UserEntity user, string passwordConfirmation, IUserRepository userRepository, string previousUserEmail)
+
+    public Dictionary<string, string> ValidatorForUpdate(UserEntity user, string passwordConfirmation,
+        IUserRepository userRepository, string previousUserEmail)
     {
         ThrowHelper.ThrowIfNull(user);
         var errors = new Dictionary<string, string>();
@@ -46,10 +50,12 @@ public class UserValidator
             {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 8 tekens lang zijn");
             }
+
             if (!user.Password.Any(char.IsUpper) || !user.Password.Any(char.IsLower))
             {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 1 hoofdletter en 1 kleine letter bevatten");
             }
+
             if (!user.Password.Any(char.IsDigit))
             {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 1 cijfer bevatten");
@@ -74,21 +80,19 @@ public class UserValidator
             }
         }
 
-        if (!string.IsNullOrWhiteSpace(user.Email) && !user.Email.Equals(previousUserEmail))
+        if (!EmailValidationRegex.IsMatch(user.Email.Trim()) ||
+            !System.Net.Mail.MailAddress.TryCreate(user.Email.Trim(), out _))
         {
-            if (!EmailValidationRegex.IsMatch(user.Email.Trim()) ||
-                !System.Net.Mail.MailAddress.TryCreate(user.Email.Trim(), out _))
-            {
-                errors.Add(nameof(user.Email), "Ongeldig email adres");
-            }
-            else if (userRepository.GetByEmail(user.Email) != null)
-            {
-                errors.Add(nameof(user.Email), "Email adres bestaat al");
-            }
+            errors.Add(nameof(user.Email), "Ongeldig email adres");
         }
+        else if (userRepository.GetByEmail(user.Email) != null)
+        {
+            errors.Add(nameof(user.Email), "Email adres bestaat al");
+        }
+
         return errors;
     }
-    
+
     public Dictionary<string, string> ValidatorForRegistration(UserEntity user, string passwordConfirmation)
     {
         ThrowHelper.ThrowIfNull(user);
@@ -105,12 +109,14 @@ public class UserValidator
             {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 8 tekens lang zijn");
             }
+
             if (!user.Password.Any(char.IsUpper) || !user.Password.Any(char.IsLower))
             {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 1 hoofdletter en 1 kleine letter bevatten");
             }
+
             if (!user.Password.Any(char.IsDigit))
-            { 
+            {
                 errorStringBuilder.AppendLine("- Wachtwoord moet minimaal 1 cijfer bevatten");
             }
 
@@ -126,7 +132,7 @@ public class UserValidator
             }
 
             string errorString = errorStringBuilder.ToString();
-            
+
             if (!string.IsNullOrEmpty(errorString))
             {
                 errors.Add(nameof(user.Password), errorString);
@@ -145,7 +151,7 @@ public class UserValidator
                 errors.Add(nameof(user.Email), "Ongeldig email adres");
             }
         }
-        
+
         return errors;
     }
 }

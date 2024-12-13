93cb6d4ffdbf363ef322344eb67abe0d20030044
Sil Gosker <silgosker@gmail.com>
2024-12-13 10:20:00 +0100

    Add history management

---
+++ Kbs.Wpf/Components/PageHistoryService.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+using Kbs.Business.Helpers;
+
+namespace Kbs.Wpf.Components;
+
+public class PageHistoryService
+{
+    private readonly Stack<Page> _pages = new();
+    private bool _navigatingBackwards;
+
+    public bool TryPush(Page page)
+    {
+        ThrowHelper.ThrowIfNull(page);
+
+        if (_pages.TryPeek(out var currentPage) && page.GetType() == currentPage.GetType())
+        {
+            return false;
+        }
+
+        _pages.Push(page);
+        _navigatingBackwards = false;
+
+        return true;
+    }
+
+    public Page Previous()
+    {
+        if (_pages.Count > 1)
+        {
+            _pages.TryPop(out var currentPage);
+        }
+
+        _pages.TryPeek(out Page previousPage);
+
+
+        _navigatingBackwards = true;
+        return previousPage;
+    }
+}

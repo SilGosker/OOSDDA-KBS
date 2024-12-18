cdc978d51bc8f91d027a5962e17766afa001fd89
Sil Gosker <silgosker@gmail.com>
2024-12-18 14:24:42 +0100

    add all entity view

--- Kbs.Web/Program.cs
+++ Kbs.Web/Program.cs
@@ -3,11 +3,15 @@ using Kbs.Business.BoatType;
 using Kbs.Business.Course;
 using Kbs.Business.Damage;
 using Kbs.Business.Game;
+using Kbs.Business.Reservation;
+using Kbs.Business.User;
 using Kbs.Data.Boat;
 using Kbs.Data.BoatType;
 using Kbs.Data.Course;
 using Kbs.Data.Damage;
 using Kbs.Data.Game;
+using Kbs.Data.Reservation;
+using Kbs.Data.User;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,8 @@ builder.Services.AddScoped<IDamageRepository, DamageRepository>();
 builder.Services.AddScoped<IBoatTypeRepository, BoatTypeRepository>();
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
 
 var app = builder.Build();
 
